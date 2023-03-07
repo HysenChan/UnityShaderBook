4e8056310f2c7644f1c21de6b5d40e3762439bff
HysenChan <HysenChan@163.com>
2023-03-07 16:00:33 +0800

    Chapter03

---
+++ Assets/Editor/Chapter3/CustomShaderGUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class CustomShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+
+        Material targetMat = materialEditor.target as Material;
+
+        bool redify = Array.IndexOf(targetMat.shaderKeywords, "REDIFY_ON")!=-1;
+        EditorGUI.BeginChangeCheck();
+        redify = EditorGUILayout.Toggle("Redify material", redify);
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (redify)
+            {
+                targetMat.EnableKeyword("REDIFY_ON");
+            }
+            else
+            {
+                targetMat.DisableKeyword("REDIFY_ON");
+            }
+        }
+    }
+}
